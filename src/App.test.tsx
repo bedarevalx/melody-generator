6541f5288d26970a7d12b2b9e66a6bc7c997ff1d
bedarevalx <bedarev.alx@gmail.com>
2023-05-04 02:33:20 +0700

    Убрал стандартный тест cra

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,6 @@ import App from './App';
 import ChordSelect from './components/ChordSelect';
 import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 it('Select changes the text after selecting a note', () => {
   const { queryByLabelText, getByLabelText } = render(<ChordSelect />);
   const selectChordAndShowSelectedChord = screen.getByRole('combobox');
