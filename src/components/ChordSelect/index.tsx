05be60b6a07085d3613490b0f87ebb4776e733b6
bedarevalx <bedarev.alx@gmail.com>
2023-05-04 02:10:29 +0700

    Написал логику селекта аккордов

--- src/components/ChordSelect/index.tsx
+++ src/components/ChordSelect/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ChordSelect = () => {
-  return <select></select>;
+  const [chord, setChord] = useState('Cmaj');
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setChord(e.target.value);
+  };
+  return (
+    <select onChange={handleChange} value={chord}>
+      <option value={'Cmaj'}>Cmaj</option>
+      <option value={'Fmaj'}>Fmaj</option>
+      <option value={'Amin'}>Amin</option>
+      <option value={'Gmaj'}>Gmaj</option>
+    </select>
+  );
 };
 
 export default ChordSelect;
