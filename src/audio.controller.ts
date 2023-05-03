d9d3a81b53b78988d8ef15e66c12a61a8a51716b
bedarevalx <bedarev.alx@gmail.com>
2023-05-04 02:45:16 +0700

    Добавил функцию, которая вернет 3 ноты по аккорду

--- src/audio.controller.ts
+++ src/audio.controller.ts
@@ -1,3 +1,13 @@
 export class AudioController {
-  getNotesByChord = () => {};
+  getNotesByChord = (chord: string) => {
+    const Chords = {
+      Cmaj: ['C', 'E', 'G'],
+      Fmaj: ['F', 'A', 'C'],
+      Amin: ['A', 'C', 'E'],
+      Gmaj: ['G', 'B', 'D'],
+    };
+
+    // @ts-ignore
+    return Chords[chord as keyof Object];
+  };
 }
