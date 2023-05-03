87eb51476bbd0a50ec566722d24c6e1aa89c8bce
bedarevalx <bedarev.alx@gmail.com>
2023-05-04 02:03:26 +0700

    Добавил пустой компонент селекта аккордов и тест для смены значения селекта

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ChordSelect from './components/ChordSelect';
+import userEvent from '@testing-library/user-event';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it('Select changes the text after selecting a note', () => {
+  const { queryByLabelText, getByLabelText } = render(<ChordSelect />);
+  const selectChordAndShowSelectedChord = screen.getByRole('combobox');
+  expect(selectChordAndShowSelectedChord).toHaveValue('Cmaj');
+  userEvent.selectOptions(selectChordAndShowSelectedChord, 'Fmaj');
+  expect(selectChordAndShowSelectedChord).toHaveValue('Fmaj');
 });
---
+++ src/components/ChordSelect/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChordSelect = () => {
+  return <select></select>;
+};
+
+export default ChordSelect;
