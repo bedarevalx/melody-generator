7af995f13717cec9aad25b0e85c993fa093475f3
bedarevalx <bedarev.alx@gmail.com>
2023-05-04 02:43:43 +0700

    Добавил тест, который проверят, что нот, полученных из аккорда 3 штуки

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import ChordSelect from './components/ChordSelect';
 import userEvent from '@testing-library/user-event';
+import { AudioController } from './audio.controller';
 
 it('Select changes the text after selecting a note', () => {
   const { queryByLabelText, getByLabelText } = render(<ChordSelect />);
@@ -23,4 +24,10 @@ it('Page rendered 4 select with chords option correctly', () => {
   expect(fmajRenderedCorrectly).toBe(4);
   expect(aminRenderedCorrectly).toBe(4);
   expect(gmajRenderedCorrectly).toBe(4);
+});
+
+it('Must return notes that consists in chord', () => {
+  const controller = new AudioController();
+  const notes = controller.getNotesByChord('Cmaj');
+  expect(notes.length).toBe(3);
 });
